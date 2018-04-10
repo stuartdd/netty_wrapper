03e517dd47155bdf0e11c787dc50b6fbf04a5fc0
stuartdd <stuart.d.davies@btinternet.com>
2018-04-10 10:46:41 +0100

    Update comments

--- src/main/java/templating/TemplateException.java
+++ src/main/java/templating/TemplateException.java
@@ -1,20 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * BT eCommerce 
- */
 package templating;
 
 /**
- * Class description
- *
- * @version $Rev: $ $Date: $
+ * @Author stuartdd
  */
 public class TemplateException extends Exception {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 4901505388413951424L;
 
     public TemplateException(String message) {
         super(message);
